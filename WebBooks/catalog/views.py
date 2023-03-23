0777665d0f5daacb3cd57e5d8f5061b18133fe28
Serghom <serghom99@gmail.com>
2023-03-23 17:22:24 +0300

    Added the ability to edit, add and remove authors

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,8 +1,11 @@
+from django.http import *
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-from  .models import Book, Author, BookInstance, Genre
+from .models import Book, Author, BookInstance, Genre
+from .forms import AuthorsForm
+
 
 
 def index(request):
@@ -22,6 +25,45 @@ def index(request):
                                                   'num_instances_available': num_instances_available,
                                                   'num_authors': num_authors,
                                                   'num_visits': num_visits})
+
+
+def authors_add(request):
+    author = Author.object.all()
+    authorsForm = AuthorsForm()
+    return render(request, "catalog/authors_add.html", {"form": authorsForm, "author": author})
+
+
+def create(request):
+    if request.method == "POST":
+        author = Author()
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+
+
+def edit1(request, id):
+    author = Author.object.get(id=id)
+    if request.method == "POST":
+        author.first_name = request.POST.get("first_name")
+        author.last_name = request.POST.get("last_name")
+        author.date_of_birth = request.POST.get("date_of_birth")
+        author.date_of_death = request.POST.get("date_of_death")
+        author.save()
+        return HttpResponseRedirect("/authors_add/")
+    else:
+        return render(request, "edit1.html", {"author": author})
+
+
+def delete(request, id):
+    try:
+        author = Author.object.get(id=id)
+        author.delete()
+        return HttpResponseRedirect("/authors_add/")
+    except Author.DoesNotExist:
+        return HttpResponseNotFound("<h2>Автор не найден</h2>")
 
 
 class BookListView(generic.ListView):
