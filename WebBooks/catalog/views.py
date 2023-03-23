e05fa2a41e7e657af502011ff0f93143b5dd9cec
Serghom <serghom99@gmail.com>
2023-03-23 11:40:39 +0300

    add num_visits

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -12,10 +12,15 @@ def index(request):
 
     num_authors = Author.object.count()
 
+    # Количество посещений этого view, подсчитаное переменой session
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     return render(request, "index.html", context={'num_books': num_books,
                                                   'num_instances': num_instances,
                                                   'num_instances_available': num_instances_available,
-                                                  'num_authors': num_authors})
+                                                  'num_authors': num_authors,
+                                                  'num_visits': num_visits})
 
 
 class BookListView(generic.ListView):
